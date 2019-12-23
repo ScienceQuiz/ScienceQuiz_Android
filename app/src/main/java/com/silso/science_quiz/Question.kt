4c18a7153fce7b71472f276602e361f36de56f55
sirasatarato <sirasatarato@gmail.com>
2019-12-24 08:17:05 +0900

    create fragment

---
+++ app/src/main/java/com/silso/science_quiz/Question.kt
@@ -0,0 +1,19 @@
+package com.silso.science_quiz
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+
+class Question: Fragment(){
+    @SuppressLint("InflateParams")
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_test, null)
+
+
+        return view
+    }
+}
