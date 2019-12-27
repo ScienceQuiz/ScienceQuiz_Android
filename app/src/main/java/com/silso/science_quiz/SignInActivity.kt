095f2ee434cc3324d31234bf69fd19b289293354
sirasatarato <sirasatarato@gmail.com>
2019-12-27 12:04:11 +0900

    Anko and sign in/up
    add Anko Library and implemented move from signIn to signup

--- app/src/main/java/com/silso/science_quiz/SignInActivity.kt
+++ app/src/main/java/com/silso/science_quiz/SignInActivity.kt
@@ -2,11 +2,17 @@ package com.silso.science_quiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_sign_in.*
+import org.jetbrains.anko.startActivity
 
 class SignInActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        go_to_signup.setOnClickListener{
+            startActivity<SignUpActivity>()
+        }
     }
 }
