4ac4e84545510e654f0de1dd197a342a121a880e
seokjun <tjrwns8024@naver.com>
2019-12-29 00:59:58 +0900

    [ADD] SignUp 로직 구현

--- app/src/main/java/com/silso/science_quiz/SignUpActivity.kt
+++ app/src/main/java/com/silso/science_quiz/SignUpActivity.kt
@@ -10,8 +10,23 @@ class SignUpActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
 
-        signin_btn.setOnClickListener{
-            finish()
+        signup_complete_btn.setOnClickListener{
+            if (isNicknameEmpty() && isIdEmpty() && isPasswordEqual() && isSchoolEmpty()){
+
+                finish()
+            }
         }
+    }
+    private fun isNicknameEmpty():Boolean{
+        return signup_name.text.isNotEmpty()
+    }
+    private fun isIdEmpty():Boolean{
+        return signup_id.text.isNotEmpty()
+    }
+    private fun isPasswordEqual():Boolean{
+        return signup_password.text.isNotEmpty() && signup_password_check.text.isNotEmpty() && signup_password_check.text == signup_password_check.text
+    }
+    private fun isSchoolEmpty():Boolean{
+        return signup_school.text.isNotEmpty()
     }
 }
