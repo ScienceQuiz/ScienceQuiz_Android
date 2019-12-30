808dda096a6a21f0b4d7d00e61a47f2dd4311efe
sirasatarato <sirasatarato@gmail.com>
2019-12-30 10:10:00 +0900

    insert texts

--- app/src/main/java/com/silso/science_quiz/Ui/activity/TestActivity.kt
+++ app/src/main/java/com/silso/science_quiz/Ui/activity/TestActivity.kt
@@ -7,22 +7,43 @@ import com.silso.science_quiz.Ui.fragment.Question
 import com.silso.science_quiz.Ui.fragment.Solution
 
 class TestActivity : AppCompatActivity() {
+    lateinit var questFragObj: Question
+    lateinit var soluFragObj: Solution
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test)
 
+        setBundle()
+        startTransaction()
+    }
+
+    fun setBundle(){
+        val bundle = Bundle()
+        bundle.putString("quest", "문제여")
+
+        questFragObj = Question()
+        questFragObj.arguments = bundle
+
+        val bundle1 = Bundle()
+        bundle1.putStringArray("solution", arrayOf("one", "two", "three", "four"))
+
+        soluFragObj = Solution()
+        soluFragObj.arguments = bundle1
+    }
+
+    fun startTransaction(){
         supportFragmentManager.beginTransaction()
             .replace(
                 R.id.question_fragment,
-                Question()
+                questFragObj
             )
             .commit()
 
         supportFragmentManager.beginTransaction()
             .replace(
                 R.id.btn_fragment,
-                Solution()
+                soluFragObj
             )
             .commit()
     }
--- app/src/main/java/com/silso/science_quiz/Ui/fragment/Question.kt
+++ app/src/main/java/com/silso/science_quiz/Ui/fragment/Question.kt
@@ -2,15 +2,25 @@ package com.silso.science_quiz.Ui.fragment
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.silso.science_quiz.R
+import kotlinx.android.synthetic.main.fragment_question.*
 
 class Question: Fragment(){
+
     @SuppressLint("InflateParams")
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_question, null)
+        val view = inflater.inflate(R.layout.fragment_question, null)
+        val bunStr = arguments?.getString("quest", "default")
+
+        val text = view.findViewById<TextView>(R.id.question)
+        text.text = bunStr
+
+        return view
     }
 }
