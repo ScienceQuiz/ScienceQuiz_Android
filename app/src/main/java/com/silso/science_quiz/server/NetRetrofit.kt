94b51270b76369f583d587fb7ecdc8c602f4a974
seokjun <tjrwns8024@naver.com>
2019-12-31 12:16:12 +0900

    server connect setting

--- app/src/main/java/com/silso/science_quiz/server/NetRetrofit.kt
+++ app/src/main/java/com/silso/science_quiz/server/NetRetrofit.kt
@@ -1,5 +1,18 @@
 package com.silso.science_quiz.server
 
-interface NetRetrofit {
+import com.silso.science_quiz.model.SignIn
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.POST
+import retrofit2.http.Query
 
+interface NetRetrofit {
+    @POST("/api/users/signup")
+    fun postSignup(@Query("nickname")nickname:String,
+                   @Query("userId")id:String,
+                   @Query("userPw")password:String,
+                   @Query("school")school:String): Call<Void>
+
+    @POST("/api/users/login")
+    fun postSignin(@Body signIn: SignIn):Call<SignIn>
 }
--- app/src/main/java/com/silso/science_quiz/server/Retrofit.kt
+++ app/src/main/java/com/silso/science_quiz/server/Retrofit.kt
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class Retrofit {
     val retrofit = Retrofit.Builder()
-        .baseUrl("")
+        .baseUrl("http://ec2-13-125-190-40.ap-northeast-2.compute.amazonaws.com:8000")
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 val service = retrofit.create(NetRetrofit::class.java)
