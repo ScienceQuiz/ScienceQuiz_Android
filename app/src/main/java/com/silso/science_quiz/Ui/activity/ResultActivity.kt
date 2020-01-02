7732d1541509e2954cff9cad1dda09925468591c
sirasatarato <sirasatarato@gmail.com>
2020-01-02 12:04:05 +0900

    Changes in movement structure
    동작 구조 변화, 결과 화면 구현

--- app/src/main/java/com/silso/science_quiz/Ui/activity/ResultActivity.kt
+++ app/src/main/java/com/silso/science_quiz/Ui/activity/ResultActivity.kt
@@ -2,12 +2,39 @@ package com.silso.science_quiz.Ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.silso.science_quiz.R
+import kotlinx.android.synthetic.main.activity_result.*
+import org.jetbrains.anko.image
 
 class ResultActivity : AppCompatActivity() {
+    var reciece = 0
+    var count = 0
+    var score = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+        setScore()
+        setImage()
+    }
+
+    fun setScore(){
+        reciece = intent.getIntExtra("correct", 0)
+        count = intent.getIntExtra("count", 0)
+
+        resultOCnt.text = reciece.toString()
+        resultXCnt.text = (count - reciece).toString()
+        score = (reciece * 100 + (count - reciece) * 50)
+        resultScoreCnt.text = score.toString()
+    }
+
+    fun setImage() {
+        resultImg.image = when {
+            score >= count * 75 -> getDrawable(R.drawable.result_happy_face)
+            score >= count * 40 -> getDrawable(R.drawable.result_ok_face)
+            else -> getDrawable(R.drawable.result_sad_face)
+        }
     }
 }
--- app/src/main/java/com/silso/science_quiz/Ui/activity/TestActivity.kt
+++ app/src/main/java/com/silso/science_quiz/Ui/activity/TestActivity.kt
@@ -9,23 +9,55 @@ import com.silso.science_quiz.R
 import com.silso.science_quiz.Ui.fragment.Question
 import com.silso.science_quiz.Ui.fragment.Solution
 import com.silso.science_quiz.data.TestData
+import com.silso.science_quiz.server.Retrofit
 import com.silso.science_quiz.util.SendAnswer
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.joinAll
+import kotlinx.coroutines.launch
+import org.jetbrains.anko.startActivity
 
 class TestActivity : AppCompatActivity(), SendAnswer {
     lateinit var questFragObj: Question
     lateinit var soluFragObj: Solution
     lateinit var data: TestData
+    var count = 10
+    var correct = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test)
 
-        recieveData()
+        //코루틴으로 서버로부터 데이터 받음
+        /*val scope = CoroutineScope(Dispatchers.Default)
+
+        scope.launch {
+            val call = Retrofit().service
+            val task1 = call.getDispatches(id, date, meal.first)
+            val task2 = call.getDispatches(id, date, meal.second)
+            val task3 = call.checkAppDetails()
+
+            joinAll(task1, task2, task3)
+
+            if (task1.isSuccessful && task2.isSuccessful && (task3.isSuccessful && task3.body().code == 1000)) {*/
+                recieveData()
+            //}
+        //}
     }
 
+    //받은 데이터로 문제 표시
     fun recieveData(){
-        data = TestData("다음 중 1인 것을 고르시오", arrayOf("Первый", "Uno", "first", "첫번째"), 2)
-        setBundle()
+        Log.e("count", count.toString())
+        if(count == 0){
+            startActivity<ResultActivity>(
+                "correct" to correct,
+                "count" to 10
+            )
+        }else {
+            count--
+            data = TestData("다음 중 1인 것을 고르시오", arrayOf("Первый", "Uno", "first", "첫번째"), 2)
+            setBundle()
+        }
     }
 
     fun setBundle(){
@@ -60,11 +92,19 @@ class TestActivity : AppCompatActivity(), SendAnswer {
     }
 
     override fun sendEvent(check: Boolean) {
-        Log.e("Успешно", check.toString())
-        Snackbar.make(window.decorView.rootView, "띄움", 1000).apply {
-            if(check) view.setBackgroundColor(Color.parseColor("#2DB400"))
-            else view.setBackgroundColor(Color.parseColor("#EB4460"))
+        var sText = "아쉽습니다...."
+        var sColor = "#EB4460"
+
+        if (check){
+            sText = "정답"
+            sColor = "#2DB400"
+            correct += 1
+        }
+
+        Snackbar.make(window.decorView.rootView, sText, 1000).apply {
+            view.setBackgroundColor(Color.parseColor(sColor))
         }.show()
+
         recieveData()
     }
 }
