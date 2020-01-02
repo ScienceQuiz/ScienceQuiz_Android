84a9e7f1dd0bde016ff8216fd4ae94934746cc64
sirasatarato <sirasatarato@gmail.com>
2020-01-02 10:51:35 +0900

    add snack bar

--- app/src/main/java/com/silso/science_quiz/Ui/activity/TestActivity.kt
+++ app/src/main/java/com/silso/science_quiz/Ui/activity/TestActivity.kt
@@ -1,34 +1,43 @@
 package com.silso.science_quiz.Ui.activity
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 import com.silso.science_quiz.R
 import com.silso.science_quiz.Ui.fragment.Question
 import com.silso.science_quiz.Ui.fragment.Solution
+import com.silso.science_quiz.data.TestData
 import com.silso.science_quiz.util.SendAnswer
 
 class TestActivity : AppCompatActivity(), SendAnswer {
     lateinit var questFragObj: Question
     lateinit var soluFragObj: Solution
+    lateinit var data: TestData
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test)
 
+        recieveData()
+    }
+
+    fun recieveData(){
+        data = TestData("다음 중 1인 것을 고르시오", arrayOf("Первый", "Uno", "first", "첫번째"), 2)
         setBundle()
     }
 
     fun setBundle(){
         val bundle = Bundle()
-        bundle.putString("quest", "문제여")
+        bundle.putString("quest", data.quest)
 
         questFragObj = Question()
         questFragObj.arguments = bundle
 
         val bundle1 = Bundle().apply {
-            putStringArray("solution", arrayOf("one", "two", "three", "four"))
-            putInt("key", 1)
+            putStringArray("solution", data.btns)
+            putInt("key", data.key)
         }
 
         soluFragObj = Solution()
@@ -52,5 +61,10 @@ class TestActivity : AppCompatActivity(), SendAnswer {
 
     override fun sendEvent(check: Boolean) {
         Log.e("Успешно", check.toString())
+        Snackbar.make(window.decorView.rootView, "띄움", 1000).apply {
+            if(check) view.setBackgroundColor(Color.parseColor("#2DB400"))
+            else view.setBackgroundColor(Color.parseColor("#EB4460"))
+        }.show()
+        recieveData()
     }
 }
---
+++ app/src/main/java/com/silso/science_quiz/data/TestData.kt
@@ -0,0 +1,3 @@
+package com.silso.science_quiz.data
+
+data class TestData(var quest: String, var btns: Array<String>, var key: Int)
